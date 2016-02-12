ba541c58597c21c9ceb8be4403151c0ea6359e4e
App Academy Student <appacademy@Apps-Mac-mini-49.local>
2016-02-12 12:22:41 -0500

    Add load scrren.

--- frontend/drummachine.jsx
+++ frontend/drummachine.jsx
@@ -1,10 +1,37 @@
 var ReactDOM = require('react-dom');
 var React = require('react');
 var Drummachine = require('./components/drummachine');
+var sounds = require('./constants/sounds');
 
 var App = React.createClass({
+
+  getInitialState: function() {
+    this.interval = window.setInterval(this.checkForLoaded, 10);
+    return {loaded: false};
+  },
+
+  checkForLoaded: function() {
+    instruments = document.getElementsByClassName('instrument');
+    for (var i = 0; i < instruments.length; i++) {
+      if (instruments[i].readyState !== 4) {
+        return false;
+      }
+    }
+    window.clearInterval(this.interval);
+    this.setState({loaded: true});
+  },
+
   render: function() {
-    return (<Drummachine />);
+    var content;
+    if (this.state.loaded) {
+      content = <Drummachine />;
+    } else {
+      content = <div className="loading">LOADING</div>;
+    }
+
+    return (
+      <div>{content}</div>
+    );
   }
 });
 
