82bfb2acbfb80dc14964b91e9393e934f7561671
App Academy Student <appacademy@Apps-Mac-mini-49.local>
2016-02-12 11:09:06 -0500

    Deprecate instrument component into button.  Still didn't fix bug with switching instrument.

--- frontend/components/button.jsx
+++ frontend/components/button.jsx
@@ -1,39 +1,41 @@
 var React = require('react');
-var Instrument = require('./instrument.jsx');
 
 var Button = React.createClass({
 
   getInitialState: function() {
     return(
-      {sounds: []}
+      {sounds: [], instruments: []}
     );
   },
 
   addInstrument: function(){
     var idx = this.state.sounds.indexOf(this.props.instrument);
     var oldSounds = this.state.sounds;
+    var oldInstruments = this.state.instruments;
     if (idx === -1) {
       oldSounds.push(this.props.instrument);
+      oldInstruments.push(document.getElementById(this.props.instrument + " instrument"));
       this.setState({on: true, sounds: oldSounds});
     } else {
       oldSounds.splice(idx, 1);
-      this.setState({on: false, sounds: oldSounds});
+      oldInstruments.splice(idx, 1);
+      this.setState({on: false, sounds: oldSounds, instruments: oldInstruments});
     }
   },
 
   componentWillReceiveProps: function(newProps) {
     if (newProps.clear) {
       this.setState({sounds: []});
+    } else if (newProps.active) {
+      this.state.instruments.forEach(function(instrument){
+      instrument.currentTime = 0;
+      instrument.play();
+      });
     }
   },
 
   render: function(){
-    var instruments = this.state.sounds.map(function(sound){
-      return (
-        <Instrument instrument={sound}
-        active={this.props.active} key={sound}/>
-      );
-    }.bind(this));
+    var instruments;
 
     var light;
     if (this.state.sounds.indexOf(this.props.instrument) !== -1) {
@@ -56,7 +58,6 @@ var Button = React.createClass({
     return(
       <div onClick={this.addInstrument} className={"button " + classname}>
         {light}
-        {instruments}
       </div>
     );
   }
